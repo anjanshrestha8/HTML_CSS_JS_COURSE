68fa1f36ceaed7ea8308bd5bcdbf05124ef2de54
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-09-06 11:51:03 +0545

    added courses of js

--- JAVA_SCRIPT/Day01_Intro/script.js
+++ JAVA_SCRIPT/Day01_Intro/script.js
@@ -1,2 +1,15 @@
 document.write("Hello World");  // writes in documnets
 console.log("Hello second time");  // writes in console
+
+
+// variable decleration
+let a=5;
+var b=10;
+let str ="Hello World! \n This is my string."
+// var c=a+b;
+
+console.log(a+b);
+document.write(a+b);
+console.log(str);
+document.write(str);
+
