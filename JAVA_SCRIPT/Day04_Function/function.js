c6938d2fea70e48fe1308a0dc17fe00bbcc21116
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-09-10 00:18:23 +0545

    subtraction code added on calculator

--- JAVA_SCRIPT/Day04_Function/function.js
+++ JAVA_SCRIPT/Day04_Function/function.js
@@ -5,7 +5,15 @@ function addition(){
     b=parseInt(b);
     var sum =a+b;
     // document.write(sum);
-    // document.getElementById("result").value=sum;
-    document.getElementById("para").innerHTML=sum;
+    document.getElementById("result").value=sum;
+    // document.getElementById("para").innerHTML=sum;
 
+}
+function subtraction(){
+    var a=document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    a=parseInt(a);
+    b=parseInt(b);
+    var sub = a-b;
+    document.getElementById("sub").value=sub;
 }
