4aa0d3725aa187876a3e01d618bbb618343b6ad0
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-09-10 12:00:31 +0545

    assignment added

--- JAVA_SCRIPT/Day05_Assignment/script.js
+++ JAVA_SCRIPT/Day05_Assignment/script.js
@@ -3,11 +3,16 @@ function finding(){
     var b = document.getElementById("second").value;
     var c = document.getElementById("third").value;
 
+    a=parseInt(a);
+    b=parseInt(b);
+    c=parseInt(c);
+
+
 
     if(a>b && a>c){
         document.getElementById("largest").value=a;
     }
-    else if(b>a&&b>c){
+    else if(b>a && b>c){
         document.getElementById("largest").value=b;
 
     }
