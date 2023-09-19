b13a07f237559ed73bc7d64d419d90fecfa1ee49
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-09-19 12:18:47 +0545

    added files for game

---
+++ PROJECTS/Game/script.js
@@ -0,0 +1,10 @@
+function player1(){
+    var a =Math.random()*1000;
+    a=parseInt(a);
+    document.getElementById("player1").value=a;
+}
+function player2(){
+    var a =Math.random()*1000;
+    a=parseInt(a);
+    document.getElementById("player1").value=a;
+}
