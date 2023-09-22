a809b89d6d60aff04d007996f2e8a02753ceac20
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-09-22 12:14:23 +0545

    multply code added

---
+++ JAVA_SCRIPT/Day06_Loops/script.js
@@ -0,0 +1,12 @@
+
+function Multiply(){
+    var a= document.getElementById("field1").Value
+    var i =1;
+    var b =0;
+    a=parseInt(a);
+    document.getElementById("result").innerHTML=a;
+    // for(i =1;i<=20;i++){
+    //     b=a*i;
+    //     document.getElementById("result").innerHTML=b;
+    // }
+}
