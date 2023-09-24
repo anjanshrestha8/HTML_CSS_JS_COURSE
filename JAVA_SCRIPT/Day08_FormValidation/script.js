ca6eaecf968ea5a06301b61ae5aea87791361d60
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-09-24 12:54:12 +0545

    form validation code done

--- JAVA_SCRIPT/Day08_FormValidation/script.js
+++ JAVA_SCRIPT/Day08_FormValidation/script.js
@@ -3,11 +3,11 @@
 function Validation(){
     var username = document.getElementById("f1");
     var password = document.getElementById("f2");
-    if(username.length<6){
+    if(username.value.length<6){
         alert("Input your Full Name");
         return false;
     }
-    else if(password.length<8){
+    else if(password.value.length<8){
         alert("Enter atleast 8 digits");
         document.getElementById("f2").value="";
         password.style.border="solid 2px Red";
